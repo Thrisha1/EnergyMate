9a8319336729d837031cf480fc944183759c4974
Thrisha <thrishakannan25@gmail.com>
2023-06-19 14:15:07 +0530

    icons  set

--- screens/DashboardScreen.jsx
+++ screens/DashboardScreen.jsx
@@ -1,11 +1,14 @@
 import { SafeAreaView,ScrollView, View, Image, Text } from "react-native";
 import React from "react";
 import Header from "../components/Header";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const DashboardScreen = () => {
   return (
     <SafeAreaView className="">
       <Header />
+      <Ionicons name="arrow-back-outline" size={32} color="" />
+
     </SafeAreaView>
   );
 };
