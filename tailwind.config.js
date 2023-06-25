13b6cbc38edab2512cbca18fdce6977a7269c627
Pavan-1802 <119658149+Pavan-1802@users.noreply.github.com>
2023-06-25 14:37:52 +0530

    feat: added the classNames for colors in tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}",
-  "./screens/**/*.{js,jsx,ts,tsx}",
-  "./components/**/*.{js,jsx,ts,tsx}",],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./screens/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'em-gray-light': '#DADADA',
+        'em-green-dark': '#006156',
+        'em-gray': '#979AA5',
+        'em-green-light':'#227E74'
+      },
+    },
   },
   plugins: [],
-}
-
+};
