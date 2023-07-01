840c7eea56e81b4c11c50daa29118ede5ca252f8
Thrisha <thrishakannan25@gmail.com>
2023-07-01 21:25:01 +0530

    documentation

--- screens/SignupScreen.jsx
+++ screens/SignupScreen.jsx
@@ -4,8 +4,26 @@ import {
 } from "react-native";
 import React from 'react'
 import { useNavigation } from "@react-navigation/native"
+import * as GoogleSignIn from 'expo-google-sign-in';
 
 const Signup = () => {
+  GoogleSignIn.initAsync({
+    clientId: 'YOUR_GOOGLE_CLIENT_ID',
+  });
+  
+  // Sign in with Google
+  const signInWithGoogle = async () => {
+    try {
+      await GoogleSignIn.askForPlayServicesAsync();
+      const { type, user } = await GoogleSignIn.signInAsync();
+      if (type === 'success') {
+        // User signed in successfully
+        console.log(user);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   const navigation = useNavigation();
   return (
     <SafeAreaView className="bg-[#4C7380] h-screen mb-0 flex-1 justify-between py-48 items-center">
